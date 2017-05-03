b459fd120385ad119f084e0b3d0ca81a1ff7b861
Esteban Calvi <estebancalvi@gmail.com>
2017-05-03 16:20:15 -0300

    Adds stylelint and autoprefixer settings

---
+++ settings/autoprefixer.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports = {
+  "browsersList": [
+    "last 2 versions",
+    "IE >= 9",
+    "Android >= 4",
+    "iOS >= 8",
+    "Chrome >= 40",
+    "Firefox >= 38",
+    "Safari >= 6",
+    "Edge >= 12",
+    "> 5%"
+  ]
+}
